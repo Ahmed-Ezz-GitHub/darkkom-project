5a787ab6dc5eb07866484cc6dedf2860445e3571
ahmed ezz <ahmed.ezz.m3@gmail.com>
2024-07-23 13:55:55 +0300

    first commit

---
+++ js/mine.js
@@ -0,0 +1,21 @@
+let but = document.getElementById("but");
+let list = document.getElementById("list");
+let raw = document.getElementById("raw");
+let non = document.getElementById("non");
+but.addEventListener("click", function () {
+    but.classList.toggle("rot");
+  list.classList.toggle("actef");
+  raw.classList.toggle("block");
+  non.classList.toggle("none");
+});
+
+
+
+
+
+
+
+
+
+
+
